021310822ef81178f81510353abdfd5b4db27d27
Denis Stetsenko <den.stetsenko@gmail.com>
2023-04-24 19:25:15 +0300

    commit

--- template-parts/single-article/post-thumbnail.php
+++ template-parts/single-article/post-thumbnail.php
@@ -1,7 +1,7 @@
 <?php
 if ( has_post_thumbnail() ) {
 	echo '<figure class="post-thumbnail figure mb-4">';
-	the_post_thumbnail( 'full', array( 'class' => 'img-fluid figure-img rounded border', 'loading' => 'lazy' ) );
+	the_post_thumbnail( 'full', array( 'class' => 'img-fluid figure-img rounded border', 'loading' => 'eager' ) );
 	echo get_the_post_thumbnail_caption() ? '<figcaption class="figure-caption font-secondary text-gray fs-6 mt-2">' . get_the_post_thumbnail_caption() . '</figcaption>' : '';
 	echo '</figure>';
 }
