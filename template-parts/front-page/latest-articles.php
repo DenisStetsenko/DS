0e5b618b22b19cc995e638f1abd736d1efd147a0
Denis Stetsenko <den.stetsenko@gmail.com>
2023-07-03 19:38:16 +0300

    disable wow animation

--- template-parts/front-page/latest-articles.php
+++ template-parts/front-page/latest-articles.php
@@ -11,11 +11,11 @@
 	<section id="front-latest-articles" class="section-margin">
 		<div class="container">
 			
-			<header class="section-title wow fadeInLeft" data-wow-delay="50ms" data-wow-offset="60" data-wow-duration="700ms">
+			<header class="section-title wow fadeInLeft" data-wow-delay="50ms" data-wow-offset="60" data-wow-duration="300ms">
 				<h2><?php _e('Read The Latest', 'wp-theme'); ?></h2>
 			</header>
 			
-			<div class="row posts-loop g-4 g-lg-5 wow fadeIn" data-wow-delay="150ms" data-wow-offset="60" data-wow-duration="700ms">
+			<div class="row posts-loop g-4 g-lg-5 wow fadeIn" data-wow-delay="150ms" data-wow-offset="60" data-wow-duration="300ms">
 				<?php while ( $wp_query->have_posts() ) : $wp_query->the_post(); ?>
 					<?php get_template_part( 'template-parts/category/post-loop', 'item', array( 'layout' => '3-cols', 'include-author-block' => 1 ) );; ?>
 				<?php endwhile; ?>
