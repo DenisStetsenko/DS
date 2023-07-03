8662ed5b6ff9ee0a785e055ee244d6a17bc8a5f4
Denis Stetsenko <den.stetsenko@gmail.com>
2023-07-03 19:26:24 +0300

    disable HERO section animation

--- template-parts/front-page/hero-intro.php
+++ template-parts/front-page/hero-intro.php
@@ -1,7 +1,7 @@
 <div id="front-hero-intro" class="bg-light-gray">
 	<div class="container">
 		<div class="row align-items-center">
-			<div class="col-lg-7 mb-6 mb-lg-0 text-center text-lg-start wow fadeInUp" data-wow-delay="100ms" data-wow-duration="700ms">
+			<div class="col-lg-7 mb-6 mb-lg-0 text-center text-lg-start">
 				<?php
 				while ( have_posts() ) {
 					the_post();
