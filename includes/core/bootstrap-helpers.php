36bcaa80aa5f35c8b5d34344cd34f2cde6e65de7
Denis Stetsenko <den.stetsenko@gmail.com>
2023-08-24 19:46:54 +0300

    fixed TOC

--- includes/core/bootstrap-helpers.php
+++ includes/core/bootstrap-helpers.php
@@ -6,26 +6,26 @@ if( ! defined('ABSPATH') ) exit;
  ***********************************************************************************************************************/
 if ( ! function_exists( 'wp_custom_theme_embed_handler_html' ) ) {
   function wp_custom_theme_embed_handler_html($cached_html, $url, $attr, $post_ID){
-
-    $classes = array();
-
-    $classes_all = array(
+	
+	  if ( ! has_blocks() ) {
+	    $classes = array();
+	
+	    $classes_all = array(
         'ratio ratio-16x9',
-    );
-
-    if (false !== strpos($url, 'vimeo.com')) {
-      $classes[] = 'vimeo';
-    }
-
-    if (false !== strpos($url, 'youtube.com')) {
-      $classes[] = 'youtube';
-    }
-
-    $classes = array_merge($classes, $classes_all);
-
-    $cached_html = str_replace('<iframe ', '<iframe class="embed-responsive-item" ', $cached_html); // First, let's add custom class
-
-    $cached_html = '<div class="' . esc_attr( implode(' ', $classes ) ) . '">' . $cached_html . '</div>';
+	    );
+	
+	    if (false !== strpos($url, 'vimeo.com')) {
+	      $classes[] = 'vimeo';
+	    }
+	
+	    if (false !== strpos($url, 'youtube.com')) {
+	      $classes[] = 'youtube';
+	    }
+	
+	    $classes = array_merge($classes, $classes_all);
+	
+	    $cached_html = '<div class="' . esc_attr( implode(' ', $classes ) ) . '">' . $cached_html . '</div>';
+	  }
 
 
     return $cached_html;
