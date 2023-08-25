4ac53462a578290d652166906bf9331b414e75cd
Denis Stetsenko <den.stetsenko@gmail.com>
2023-08-25 20:18:50 +0300

    entry-heading styling

--- template-parts/single-article/entry-header.php
+++ template-parts/single-article/entry-header.php
@@ -1,13 +1,19 @@
-<header class="entry-header">
-	<?php the_title('<h1 class="entry-title mb-2">', '</h1>'); ?>
-	<?php
-	$subtitle = get_field( 'subtitle' );
-	if ( ! $subtitle && has_excerpt() ) {
-		$excerpt = get_the_excerpt();
-		$excerpt = wp_strip_all_tags($excerpt);
-		$subtitle = $excerpt;
-	}
-	if ( $subtitle ) : ?>
-		<p class="subtitle font-secondary mb-0 fw-lighter"><?= wp_strip_all_tags($subtitle); ?></p>
-	<?php endif; ?>
-</header>
+<div class="single-entry-heading">
+	<div class="row">
+		<div class="col-lg-8">
+			<header class="entry-header">
+				<?php the_title('<h1 class="entry-title mb-2">', '</h1>'); ?>
+				<?php
+				$subtitle = get_field( 'subtitle' );
+				if ( ! $subtitle && has_excerpt() ) {
+					$excerpt = get_the_excerpt();
+					$excerpt = wp_strip_all_tags($excerpt);
+					$subtitle = $excerpt;
+				}
+				if ( $subtitle ) : ?>
+					<p class="subtitle font-secondary mb-0 fw-lighter"><?= wp_strip_all_tags($subtitle); ?></p>
+				<?php endif; ?>
+			</header>
+		</div>
+	</div>
+</div>
