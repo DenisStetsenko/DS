8432b29b6d08f1a50f1797cdc0f8d50935735f83
Denis Stetsenko <den.stetsenko@gmail.com>
2023-08-26 14:41:08 +0300

    set post layout visually different

--- includes/core/wp-admin-post-columns.php
+++ includes/core/wp-admin-post-columns.php
@@ -12,8 +12,8 @@ add_filter( "manage_post_posts_columns", function ( $columns ) {
 	
 	return array_merge ( $columns, array (
 		'title'       => __ ('Title'),
-		'author'      => __ ('Author'),
 		'post-layout' => __ ('Layout'),
+		'author'      => __ ('Author'),
 		'categories'  => __ ('Categories'),
 		'date'        => __('Date')
 //	'designation' => __ ( 'Designation' ),
@@ -29,9 +29,9 @@ add_action( "manage_post_posts_custom_column", function ( $column_name, $post_id
 	if ( $column_name == 'post-layout' ) {
 		$template = get_post_meta( $post_id, '_wp_page_template', true );
 		if ( $template === 'default' || $template === '' ) {
-			$icon = '<span class="dashicons dashicons-table-col-before"></span>';
+			$icon = '<span class="informational-post-layout" style="color: #a225ea; font-weight: 600">Info Post</span>';
 		} else {
-			$icon = '<span class="dashicons dashicons-image-flip-horizontal"></span>';
+			$icon = '<span class="list-post-layout" style="color: #11b264; font-weight: 600">List Post</span>';
 		}
 		
 		// Display an ACF Icon field
@@ -43,7 +43,7 @@ add_action( "manage_post_posts_custom_column", function ( $column_name, $post_id
 
 add_action( 'admin_print_styles-edit.php', function() {
 	echo '<style>.fixed #author.column-author{width: 70px;}
-								 #post-layout{ width: 60px; text-align: center }
+								 #post-layout{ width: 130px; text-align: center }
  								 #post-layout .column-thumb img{ max-width: 100%; height: auto }
  								 .post-layout.column-post-layout { text-align: center }
         </style>';
