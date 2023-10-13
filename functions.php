3fff3b761b56a4e2ad0735bd742ba27289d3a410
Denis <den.stetsenko@gmail.com>
2023-10-13 17:54:27 +0200

    Moved RankMath Area to the Bottom on Block Editor screen

--- functions.php
+++ functions.php
@@ -413,3 +413,10 @@ function retitle($match) {
  * @param boolean $unsigned Enable cache or not, defaults to true
  */
 add_filter( 'rank_math/sitemap/enable_caching', '__return_false');
+
+
+/**
+ * Disable Gutenberg Sidebar Integration
+ * Use this filter to remove Rank Math integration from the Gutenberg Sidebar and add old meta boxes below the content area.
+ */
+add_filter( 'rank_math/gutenberg/enabled', '__return_false' );
