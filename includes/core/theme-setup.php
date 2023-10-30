00e52eb5ee51fd539dfb5ec54d3f50c089a1d4d7
Denis Stetsenko <den.stetsenko@gmail.com>
2023-10-30 18:49:01 +0200

    added Lora 600

--- includes/core/theme-setup.php
+++ includes/core/theme-setup.php
@@ -50,7 +50,7 @@ if ( ! function_exists('wp_custom_theme_setup') ) {
 		
 	  // Editor Style
 	  add_theme_support( 'editor-styles' );
-	  add_editor_style( array( 'https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&family=Lora:ital,wght@0,400;0,700;1,400;1,700&display=swap', 'style-editor.css' ));
+	  add_editor_style( array( 'https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&family=Lora:ital,wght@0,400;0,600;0,700;1,400;1,600;1,700&display=swap', 'style-editor.css' ));
 		
 		// new image sizes
 	  add_image_size( 'top-picks-thumbnail', 130, 130 );
