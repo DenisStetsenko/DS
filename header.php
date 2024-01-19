091dfa3b3a1b416ffff5454f6f5665e9496809a4
Denis Stetsenko <den.stetsenko@gmail.com>
2024-01-19 23:23:35 +0200

    Add Google TM

--- header.php
+++ header.php
@@ -8,6 +8,14 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
+	<!-- Google Tag Manager -->
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-NS5BDJ2');</script>
+	<!-- End Google Tag Manager -->
+	
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<!--[if IE]><meta http-equiv='X-UA-Compatible' content='IE=edge,chrome=1'><![endif]-->
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -21,6 +29,10 @@
 </head>
 
 <body <?php body_class(); ?> itemscope itemtype="https://schema.org/WebPage">
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NS5BDJ2"
+									height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 
 <?php get_template_part('template-parts/header/offcanvas-mobile-menu'); ?>
 
