e14d9dc35e236854489cf2443e0b70c64b975c54
Denis Stetsenko <den.stetsenko@gmail.com>
2024-01-19 22:16:30 +0200

    update fancybox script

--- functions.php
+++ functions.php
@@ -113,8 +113,8 @@ if ( ! function_exists( 'wp_custom_scripts_and_styles' ) ) {
 			wp_enqueue_script('fancybox', 'https://cdnjs.cloudflare.com/ajax/libs/fancyapps-ui/5.0.33/fancybox/fancybox.umd.js', array(), null, array( 'in_footer' => true, 'strategy'  => 'defer' ) );
 			wp_add_inline_script( 'fancybox',
 				"document.addEventListener('DOMContentLoaded', () => {
-					Fancybox.bind('[data-fancybox], .entry-content a[href$=\"png\"], .entry-content a[href$=\"jpg\"]', {
-						infinite: false
+					Fancybox.bind('[data-fancybox], .entry-content a[href$=\".png\"], .entry-content a[href$=\".jpg\"]', {
+							infinite: false
 					});
 				});"
 			);
