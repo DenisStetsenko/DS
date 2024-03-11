d38ef080c4b268a0b894f7656bb83555d43add74
Denis Stetsenko <den.stetsenko@gmail.com>
2024-03-12 00:47:02 +0200

    define fixed values for featured post thumbnail

--- template-parts/single-article/post-thumbnail.php
+++ template-parts/single-article/post-thumbnail.php
@@ -1,7 +1,7 @@
 <?php
 if ( has_post_thumbnail() ) {
 	echo '<figure class="post-thumbnail figure mb-5 mb-lg-6 w-100">';
-	the_post_thumbnail( 'full', array( 'class' => 'img-fluid figure-img rounded border w-100', 'loading' => 'eager' ) );
+	the_post_thumbnail( array(850,9999), array( 'class' => 'img-fluid figure-img rounded border w-100', 'loading' => 'eager', 'width' => 100, 'height' => 100 ) );
 	echo get_the_post_thumbnail_caption() ? '<figcaption class="figure-caption font-secondary text-gray fs-6 mt-2">' . get_the_post_thumbnail_caption() . '</figcaption>' : '';
 	echo '</figure>';
 }
